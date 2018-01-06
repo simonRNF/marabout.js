b6a4971e9f153b807c4495465eae1fd427795aee
Simon <simonrnf17@outlook.fr>
2018-01-07 00:15:47 +0100

    commit #1

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <html>
 <head>
   <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+  <script src="simon.js"></script>
   <script src="sofiane.js"></script>
   <script src="corentin.js"></script>
-  <script src="simon.js"></script>
 </head>
 <body>
 
 
 
-<div class="marker">Salut, c'estnous !</div>
+<div class="marker">Salut, c'est nous !</div>
 
 
 
---
+++ simon.js
@@ -0,0 +1,5 @@
+function marabout__init(){ // We check if jquery is loaded before marabout.js library
+  if(!(window.jQuery)){
+    alert("You must load Jquery before calling marabout.js");
+    return false;
+  }}
