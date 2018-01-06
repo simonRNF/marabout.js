0fd57532f2d02715a292fbb5fa1bb2d7035e420b
Simon <simonrnf17@outlook.fr>
2018-01-07 00:05:34 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+  <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.min.js"></script>
+  <script src="sofiane.js"></script>
+  <script src="corentin.js"></script>
+  <script src="simon.js"></script>
+</head>
+<body>
+
+
+
+<div class="marker">Salut, c'estnous !</div>
+
+
+
+</body>
+</html>
